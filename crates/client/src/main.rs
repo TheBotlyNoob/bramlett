d0e626762f997a11feca5ef9c81a8e7d4a516daa
Jay Jackson <jj@jj.is-a.dev>
2023-10-15 14:13:09 -0700

    fix window sizing

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -12,8 +12,8 @@ async fn main() -> eframe::Result<()> {
     // Log to stderr (if you run with `RUST_LOG=debug`).
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let native_options = eframe::NativeOptions {
-        initial_window_size: Some([400.0, 300.0].into()),
-        min_window_size: Some([300.0, 220.0].into()),
+        initial_window_size: Some([350.0, 750.0].into()),
+        min_window_size: Some([350.0, 750.0].into()),
         ..Default::default()
     };
     eframe::run_native(
