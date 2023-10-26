76eb2f6f96228c8af1d87250a3dec3142c9fb09f
Jay D Jackson <jj@jj.is-a.dev>
2023-10-26 21:13:11 +0000

    use latest version of rspc

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,92 +1,212 @@
-use std::{path::PathBuf, sync::Arc, time::Duration};
+use std::{
+    net::{Ipv6Addr, SocketAddr},
+    path::PathBuf,
+    pin::Pin,
+    sync::atomic::AtomicU16,
+    task::{Context, Poll},
+    time::Duration,
+};
 
 use async_stream::stream;
-use axum::{
-    http::{HeaderValue, Method},
-    routing::get,
-};
-use rspc::{integrations::httpz::Request, Config, Router};
-use tokio::time::sleep;
-use tower_http::cors::{AllowOrigin, CorsLayer};
+use axum::routing::get;
+use futures::{Stream, StreamExt};
+use rspc::{ExportConfig, Rspc};
+use serde::Serialize;
+use specta::Type;
+use tokio::{sync::broadcast, time::sleep};
+use tower_http::cors::{Any, CorsLayer};
+use tracing::info;
 
-struct Ctx {}
+#[derive(Clone)]
+struct Ctx {
+    x_demo_header: Option<String>,
+}
 
-fn router() -> Arc<Router<Ctx>> {
-    static ROUTER: once_cell::sync::Lazy<Arc<Router<Ctx>>> = once_cell::sync::Lazy::new(|| {
-        rspc::Router::<Ctx>::new()
-            .config(Config::new().export_ts_bindings(
-                PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../bindings.ts"),
-            ))
-            .query("version", |t| t(|_, _: ()| env!("CARGO_PKG_VERSION")))
-            .query("echo", |t| t(|_, v: String| v))
-            .query("error", |t| {
-                t(|_, _: ()| {
-                    Err(rspc::Error::new(
-                        rspc::ErrorCode::InternalServerError,
-                        "Something went wrong".into(),
-                    )) as Result<String, rspc::Error>
-                })
-            })
-            .query("transformMe", |t| t(|_, _: ()| "Hello, world!".to_string()))
-            .mutation("sendMsg", |t| {
-                t(|_, v: String| {
-                    println!("Client said '{}'", v);
-                    v
-                })
-            })
-            .subscription("pings", |t| {
-                t(|_ctx, _args: ()| {
-                    stream! {
-                        println!("Client subscribed to 'pings'");
-                        for i in 0..5 {
-                            println!("Sending ping {}", i);
-                            yield "ping".to_string();
-                            sleep(Duration::from_secs(1)).await;
-                        }
-                    }
-                })
-            })
-            // TODO: Results being returned from subscriptions
-            // .subscription("errorPings", |t| t(|_ctx, _args: ()| {
-            //     stream! {
-            //         for i in 0..5 {
-            //             yield Ok("ping".to_string());
-            //             sleep(Duration::from_secs(1)).await;
-            //         }
-            //         yield Err(rspc::Error::new(ErrorCode::InternalServerError, "Something went wrong".into()));
-            //     }
-            // }))
-            .build()
-            .arced()
-    });
-    ROUTER.clone()
+#[derive(thiserror::Error, Serialize, Type, Debug)]
+#[error("{0}")]
+struct Error(&'static str);
+
+const R: Rspc<Ctx, Error> = Rspc::new();
+
+#[derive(thiserror::Error, serde::Serialize, specta::Type, Debug)]
+pub enum MyCustomError {
+    #[error("I am broke")]
+    IAmBroke,
 }
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let router = R
+        .router()
+        .procedure(
+            "version",
+            R.with(|mw, ctx| async move {
+                mw.next(ctx).map(|resp| async move {
+                    println!("Client requested version '{}'", resp);
+                    resp
+                })
+            })
+            .with(|mw, ctx| async move { mw.next(ctx) })
+            .query(|_, _: ()| {
+                info!("Client requested version");
+                Ok(env!("CARGO_PKG_VERSION"))
+            }),
+        )
+        .procedure(
+            "X-Demo-Header",
+            R.query(|ctx, _: ()| Ok(ctx.x_demo_header.unwrap_or_else(|| "No header".to_string()))),
+        )
+        .procedure("echo", R.query(|_, v: String| Ok(v)))
+        .procedure("echo2", R.query(|_, v: String| async move { Ok(v) }))
+        .procedure(
+            "error",
+            R.query(|_, _: ()| Err(Error("Something went wrong")) as Result<String, _>),
+        )
+        .procedure(
+            "error",
+            R.mutation(|_, _: ()| Err(Error("Something went wrong")) as Result<String, _>),
+        )
+        .procedure(
+            "transformMe",
+            R.query(|_, _: ()| Ok("Hello, world!".to_string())),
+        )
+        .procedure(
+            "sendMsg",
+            R.mutation(|_, v: String| {
+                println!("Client said '{}'", v);
+                Ok(v)
+            }),
+        )
+        .procedure(
+            "pings",
+            R.subscription(|_, _: ()| {
+                println!("Client subscribed to 'pings'");
+                stream! {
+                    yield Ok("start".to_string());
+                    for i in 0..5 {
+                        info!("Sending ping {}", i);
+                        yield Ok(i.to_string());
+                        sleep(Duration::from_secs(1)).await;
+                    }
+                }
+            }),
+        )
+        .procedure(
+            "errorPings",
+            R.subscription(|_ctx, _args: ()| {
+                stream! {
+                    for _ in 0..5 {
+                        yield Ok("ping".to_string());
+                        sleep(Duration::from_secs(1)).await;
+                    }
+                    yield Err(Error("Something went wrong"));
+                }
+            }),
+        )
+        .procedure(
+            "testSubscriptionShutdown",
+            R.subscription({
+                static COUNT: AtomicU16 = AtomicU16::new(0);
+                |_, _: ()| {
+                    let id = COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+
+                    pub struct HandleDrop {
+                        id: u16,
+                        sent: bool,
+                    }
+
+                    impl Stream for HandleDrop {
+                        type Item = u16;
+
+                        fn poll_next(
+                            mut self: Pin<&mut Self>,
+                            _: &mut Context<'_>,
+                        ) -> Poll<Option<Self::Item>> {
+                            if self.sent {
+                                Poll::Ready(None)
+                            } else {
+                                self.sent = true;
+                                Poll::Ready(Some(self.id))
+                            }
+                        }
+                    }
+
+                    impl Drop for HandleDrop {
+                        fn drop(&mut self) {
+                            println!("Dropped subscription with id {}", self.id);
+                        }
+                    }
+
+                    HandleDrop { id, sent: false }.map(Ok)
+                }
+            }),
+        )
+        .procedure(
+            "customErr",
+            R.error::<MyCustomError>()
+                .query(|_, _args: ()| Err::<(), _>(MyCustomError::IAmBroke)),
+        )
+        .procedure("batchingTest", {
+            let (tx, _) = broadcast::channel(10);
+
+            tokio::spawn({
+                let tx = tx.clone();
+
+                async move {
+                    let mut timer = tokio::time::interval(Duration::from_secs(1));
+                    loop {
+                        timer.tick().await;
+                        tx.send("ping".to_string()).ok();
+                    }
+                }
+            });
+
+            R.subscription(move |_, _: ()| {
+                let mut rx = tx.subscribe();
+                stream! {
+                    while let Ok(msg) = rx.recv().await {
+                        yield Ok(msg);
+                    }
+                }
+            })
+        })
+        .build()
+        .unwrap()
+        .arced(); // This function is a shortcut to wrap the router in an `Arc`.
+
+    router
+        .export_ts(ExportConfig::new(
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../bindings.ts"),
+        ))
+        .unwrap();
+
     // We disable CORS because this is just an example. DON'T DO THIS IN PRODUCTION!
     let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
-        .allow_origin(AllowOrigin::list([
-            HeaderValue::from_static("http://localhost:3000"),
-            HeaderValue::from_static("http://127.0.0.1:3000"),
-        ]));
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_origin(Any);
 
     let app = axum::Router::new()
         .route("/", get(|| async { "Hello 'rspc'!" }))
         .nest(
             "/rspc",
-            router()
-                .endpoint(|req: Request| {
-                    println!("Client requested operation '{}'", req.uri().path());
-                    Ctx {}
-                })
-                .axum(),
+            rspc_httpz::endpoint(router, |req: rspc_httpz::Request| {
+                println!("Client requested operation '{}'", req.uri().path());
+                Ctx {
+                    x_demo_header: req
+                        .headers()
+                        .get("X-Demo-Header")
+                        .map(|v| v.to_str().unwrap().to_string()),
+                }
+            })
+            .axum(),
         )
         .layer(cors);
 
-    let addr = "[::]:4000".parse::<std::net::SocketAddr>().unwrap(); // This listens on IPv6 and IPv4
-    println!("listening on http://{}/rspc/version", addr);
+    let addr = SocketAddr::from((Ipv6Addr::UNSPECIFIED, 4000));
+    println!("listening on http://{}/rspc", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
