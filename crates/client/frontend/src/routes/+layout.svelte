b63acfcef3dee511cc2cc22486e83c1cd7d7b722
TheBotlyNoob <jj@jj.is-a.dev>
2023-11-14 20:13:36 -0500

    fix  build

--- crates/client/frontend/src/routes/+layout.svelte
+++ crates/client/frontend/src/routes/+layout.svelte
@@ -1,15 +1,6 @@
 <script>
 	import '@fontsource/poppins';
 	import '$lib/app.css';
-
-    import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
-	import { dev } from '$app/environment';
-
-
-    if (dev) {
-        loadDevMessages();
-        loadErrorMessages();
-    }
 </script>
 
 <slot />
